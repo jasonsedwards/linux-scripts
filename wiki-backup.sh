7ae42eea0309d0dbd6325e60ad43f6b5f5e66797
Jason Edwards <jason@stuart-edwards.info>
2015-09-07 22:57:47 +0100

    Edited destination directory it is now /tmp/dokuwiki/

--- wiki-backup.sh
+++ wiki-backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 set fp [open "/root/wiki_password" r]
 set pass_wd [read $fp]
-spawn rsync -av jason.edwards@wiki.stuart-edwards.info:/usr/share/nginx/html/dokuwiki/ /tmp
+spawn rsync -av jason.edwards@wiki.stuart-edwards.info:/usr/share/nginx/html/dokuwiki/ /tmp/dokuwiki/
 expect "password:"
 send "$pass_wd\n"
 expect eof
