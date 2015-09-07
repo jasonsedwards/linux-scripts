0a2e8a3c80c86e195caf38a59a36743b1e51fa05
Jason Edwards <jason@stuart-edwards.info>
2015-09-07 22:08:08 +0100

    Added script to backup my wiki to my home linux server.

---
+++ wiki-backup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/expect
+set fp [open "/home/jason/wiki_password" r]
+set pass_wd [read $fp]
+spawn rsync -av jason.edwards@wiki.stuart-edwards.info:/usr/share/nginx/html/dokuwiki/ /tmp
+expect "password:"
+send "$pass_wd\n"
+expect eof
+if [catch wait] {
+    puts "rsync failed"
+    exit 1
+}
+exit 0
