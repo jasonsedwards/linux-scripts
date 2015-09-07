55a0ffbe427c5da85264e420666d54b45f212e8f
Jason Edwards <jason@stuart-edwards.info>
2015-09-07 22:43:06 +0100

    Moved job over to root.

--- wiki-backup.sh
+++ wiki-backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-set fp [open "/home/jason/wiki_password" r]
+set fp [open "/root/wiki_password" r]
 set pass_wd [read $fp]
 spawn rsync -av jason.edwards@wiki.stuart-edwards.info:/usr/share/nginx/html/dokuwiki/ /tmp
 expect "password:"
